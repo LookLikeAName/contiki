5d4cfd2af337cddf8c07cf86345c26a32d7aff1d
LookLikeAName <yl301l4ul6@gmail.com>
2018-10-25 18:21:15 +0800

    add debug message

--- core/net/ipv6/sicslowpan.c
+++ core/net/ipv6/sicslowpan.c
@@ -725,8 +725,9 @@ compress_hdr_iphc(linkaddr_t *link_destaddr)
     uint8_t ors;
     ors = (orchestra_request_slots_for_root & 0x0F);
     UIP_IP_BUF->tcflow = (ors << 4) | UIP_IP_BUF->tcflow;
+    PRINTF("UIP_IP_BUF->tcflow : %02x , ors: %02x \n",UIP_IP_BUF->tcflow,ors);
   }
-  PRINTF("UIP_IP_BUF->tcflow : %02x , ors: %02x\n",UIP_IP_BUF->tcflow,ors);
+  
   
   tmp = (UIP_IP_BUF->vtc << 4) | (UIP_IP_BUF->tcflow >> 4);
   tmp = ((tmp & 0x03) << 6) | (tmp >> 2);
