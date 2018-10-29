96b008d6d1715c6f3fcc19d598155e1168948e6a
LookLikeAName <yl301l4ul6@gmail.com>
2018-10-29 17:08:48 +0800

    debug

--- apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-grouped-slotframe.c
+++ apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-grouped-slotframe.c
@@ -141,7 +141,12 @@ select_packet(uint16_t *slotframe, uint16_t *timeslot)
       *slotframe = slotframe_handle;
     }
     if(timeslot != NULL) {
-      *timeslot = get_node_timeslot(dest);
+      /*DEBUG */
+      srand(time(NULL));
+      int rnd;
+      rnd=(rand()%3);
+      
+      *timeslot = get_node_timeslot(dest)+rnd;
       groups[get_group_offset(dest)].allocate_slot_offset=(groups[get_group_offset(dest)].allocate_slot_offset+1)%groups[get_group_offset(dest)].required_slot;
     }
     PRINTF("PACKETBUF_ATTR_TSCH_SLOTFRAME: %02x,PACKETBUF_ATTR_TSCH_TIMESLOT: %02x\n",*slotframe,*timeslot);
