0f2f0797ea83d39f44afdfa76442b82b5aea5242
LookLikeAName <yl301l4ul6@gmail.com>
2018-10-30 11:41:15 +0800

    chang piggybag method to make sure net stack

--- core/net/ipv6/sicslowpan.c
+++ core/net/ipv6/sicslowpan.c
@@ -724,7 +724,7 @@ compress_hdr_iphc(linkaddr_t *link_destaddr)
   if(orchestra_request_slots_for_root != 0 && orchestra_request_slots_for_root != NULL){
     uint8_t ors;
     ors = (orchestra_request_slots_for_root & 0x0F);
-    UIP_IP_BUF->tcflow = (ors << 4) | (UIP_IP_BUF->tcflow & 0x0F);
+    UIP_IP_BUF->tcflow = (ors << 4) | UIP_IP_BUF->tcflow ;
     PRINTF("UIP_IP_BUF->tcflow : %02x , ors: %02x \n",UIP_IP_BUF->tcflow,ors);
   }
   
@@ -1017,7 +1017,8 @@ uncompress_hdr_iphc(uint8_t *buf, uint16_t ip_len)
         SICSLOWPAN_IP_BUF(buf)->flow = 0;
       }
     }
-    orchestra_requested_slots_frome_child = (SICSLOWPAN_IP_BUF(buf)->tcflow & 0xF0) >> 4;
+    orchestra_requested_slots_frome_child = ((SICSLOWPAN_IP_BUF(buf)->tcflow & 0xF0) >> 4)+1;
+    SICSLOWPAN_IP_BUF(buf)->tcflow = (SICSLOWPAN_IP_BUF(buf)->tcflow & 0x0F);
     PRINTF("orchestra_requested_slots_frome_child: %02x \n",orchestra_requested_slots_frome_child);
   /* Next Header */
   if((iphc0 & SICSLOWPAN_IPHC_NH_C) == 0) {
