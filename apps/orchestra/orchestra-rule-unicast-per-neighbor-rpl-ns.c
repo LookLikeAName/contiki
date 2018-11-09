277ed3c5a07b32478bd620536cd71de43d0b0aed
LookLikeAName <yl301l4ul6@gmail.com>
2018-11-09 11:27:19 +0800

    debug

--- apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns.c
+++ apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns.c
@@ -94,7 +94,7 @@ select_packet(uint16_t *slotframe, uint16_t *timeslot)
   /* Select data packets we have a unicast link to */
   const linkaddr_t *dest = packetbuf_addr(PACKETBUF_ADDR_RECEIVER);
   if(packetbuf_attr(PACKETBUF_ATTR_FRAME_TYPE) == FRAME802154_DATAFRAME
-     && !linkaddr_cmp(dest, &linkaddr_null)&&!is_time_source(dest)) {
+     && !linkaddr_cmp(dest, &linkaddr_null)) {
     if(slotframe != NULL) {
       *slotframe = slotframe_handle;
     }
