4f3b09345e73cf49f14acfab08e7878677c962d1
LookLikeAName <yl301l4ul6@gmail.com>
2018-11-13 17:15:29 +0800

    debug

--- apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-grouped-slotframe.c
+++ apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-grouped-slotframe.c
@@ -238,7 +238,6 @@ void slot_request_acked(){
     parent_group_offset = get_group_offset(&orchestra_parent_linkaddr);
     groups[parent_group_offset].required_slot = orchestra_request_slots_for_root;
     PRINTF("slot_request_acked %d\n", groups[parent_group_offset].required_slot);
-    orchestra_request_slots_for_root=0;
   }
 }
 
@@ -247,7 +246,8 @@ get_request_slots_for_root(linkaddr_t *dest)
 {
   /* Select data packets we have a unicast link to */
   PRINTF("get_request_slots_for_root %d ,%d\n", orchestra_request_slots_for_root,is_time_source(dest));
-  if(!linkaddr_cmp(dest, &linkaddr_null) && is_time_source(dest)) {
+  if(packetbuf_attr(PACKETBUF_ATTR_FRAME_TYPE) == FRAME802154_DATAFRAME
+     && !linkaddr_cmp(dest, &linkaddr_null) && is_time_source(dest)) {
       PRINTF("get_request_slots_for_root is parent%d\n", orchestra_request_slots_for_root);
     return orchestra_request_slots_for_root;
   }
