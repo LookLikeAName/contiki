504d2370ef92baea0f310ca905dd5786e06e557b
LookLikeAName <yl301l4ul6@gmail.com>
2018-11-22 17:40:18 +0800

    debug

--- apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-grouped-slotframe.c
+++ apps/orchestra/orchestra-rule-unicast-per-neighbor-rpl-ns-grouped-slotframe.c
@@ -360,9 +360,9 @@ void self_rx_maintain(const struct tsch_link *link,uint8_t packet_receved,int fr
    {
     last_rx_countdown = ORCHESTRA_LAST_RX_UNUESD_DELETE_THRESHOLD;
    }
-   
+   printf("self_rx_maintain: %d , %d ,%d \n",last_rx_countdown,packet_receved,frame_valid);
   }
-  PRINTF("self_rx_maintain: %d , %d ,%d \n",last_rx_countdown,packet_receved,frame_valid);
+  
 }
 #endif
 /*---------------------------------------------------------------------------*/
