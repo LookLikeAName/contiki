245fc1579f66d819862161ce9eb3a59b30a7930c
LookLikeAName <yl301l4ul6@gmail.com>
2019-02-12 16:03:33 +0800

    link test

--- core/net/mac/tsch/tsch-slot-operation.c
+++ core/net/mac/tsch/tsch-slot-operation.c
@@ -1029,7 +1029,7 @@ PT_THREAD(tsch_slot_operation(struct rtimer *t, void *ptr))
             printf("TSCH no backup link\n");
             break;
           }
-          printf("TSCH get backup link: %d %d %d\n",i, backup_link[i]->slotframe_handle,sizeof(backup_link)/sizeof(tsch_link));
+          printf("TSCH get backup link: %d %d\n",i, backup_link[i]->slotframe_handle);
           current_link = backup_link[i];
           current_packet = get_packet_and_neighbor_for_link(current_link, &current_neighbor);
           if(current_packet != NULL)
